a2b1db484f02f0a0a9645c2a13352e8d9db88e2f
IgnacioPagella <nacho.pagella@gmail.com>
2022-05-27 14:31:16 -0300

    borrador

---
+++ Assets/Personaje.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float jumpForce;
+    public int maxJumps;
+
+    int hasJump;
+
+    RigidBody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
