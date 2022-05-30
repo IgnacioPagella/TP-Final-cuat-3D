9f974fbb7c35cdc7e3e52694e48385a5b3669b23
IgnacioPagella <nacho.pagella@gmail.com>
2022-05-30 16:54:31 -0300

    dobla y salta

--- Assets/Personaje.cs
+++ Assets/Personaje.cs
@@ -10,17 +10,45 @@ public class Personaje : MonoBehaviour
     public int maxJumps;
 
     int hasJump;
+    Rigidbody rb;
 
-    RigidBody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hasJump = maxJumps;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotationSpeed, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && hasJump > 0)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            hasJump--;
+        }
+        void OnCollisionEnter(Collision col)
+        {
+            if(col.gameObject.tag == "ground")
+            {
+                hasJump = maxJumps;
+            }
+        }
     }
 }
