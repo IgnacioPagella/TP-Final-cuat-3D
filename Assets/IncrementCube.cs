464bbfae39cbd9b520e395fd0e057c160632fa6c
IgnacioPagella <nacho.pagella@gmail.com>
2022-06-13 14:23:15 -0300

    Movement Obstacles

---
+++ Assets/IncrementCube.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncrementCube : MonoBehaviour
+{
+    bool Comprobante;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Comprobante)
+        {
+            transform.localScale += new Vector3(0, 0.05f, 0);
+            if(transform.localScale.y > 4)
+            {
+                Comprobante = false;
+            }
+        }
+        if(Comprobante == false)
+        {
+            transform.localScale -= new Vector3(0, 0.05f, 0);
+            if (transform.localScale.y < 0.8f)
+            {
+                Comprobante = true;
+            }
+        }
+        
+    }
+}
---
+++ Assets/MovementBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBar : MonoBehaviour
+{
+    bool Comprobante;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Comprobante = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Comprobante)
+        {
+            transform.position -= new Vector3(speed, 0, 0);
+            if(transform.position.x <=(-32))
+            {
+                Comprobante = false;
+            }
+        }
+        if (Comprobante == false)
+        {
+            transform.position += new Vector3(speed, 0, 0);
+            if (transform.position.x >= (-20))
+            {
+                Comprobante = true;
+
+            }
+        }
+        
+    }
+}
